7b289a266c196645585b493df9a12823ffe9d013
AbdullahJawaid <91978361+AbdullahJawaid@users.noreply.github.com>
2023-04-30 02:58:49 +0500

    Initial commit

--- restaurant-app/src/Components/Basics/Restaurant.js
+++ restaurant-app/src/Components/Basics/Restaurant.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react'
-import Data from './data.js'
-import  './Style.css'
-import MenuList from '../MenuList';
+import React, { useState } from "react";
+import "./Style.css";
+import Data from "./data.js";
+import MenuCard from "./MenuList";
+import Navbar from "./Navbar";
 
-function Restaurant() {
+const uniqueList = [
+  ...new Set(
+    Data.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
 
-  const [menuData,setMenuData]=useState(Data);
+// console.log(uniqueList);
 
+const Resturant = () => {
+  const [menuData, setMenuData] = useState(Data);
+  const [menuList, setMenuList] = useState(uniqueList);
 
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Data);
+      return;
+    }
+
+    const updatedList = Data.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
 
   return (
     <>
-     <MenuList
-     menuData={menuData}
-     />
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
     </>
-  )
-}
+  );
+};
 
-export default Restaurant
+export default Resturant;
